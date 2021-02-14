2832ee997cbfc1bfd96184593741998fac3cf8ef
Sarah Brand <brand.s@me.com>
2021-02-14 16:57:36 +1100

    adding in source folder

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,55 @@
+var quizQuestions = [
+    {
+        question: "Do you understand that you rated that dog 3 out of 5?",
+        answers: [
+            {
+                type: "Love",
+                content: "What no I'm sorry that was a mistake I meant 5/5 obviously"
+            },
+            {
+                type: "Hate",
+                content: "No I meant 1 out of 5 beacuse I hate the world"
+            },
+            {
+                type: "Middle",
+                content: "Yes. Why do you care so much about my dog rating weirdo"
+            }
+        ]
+    },
+    {
+        question: "True or False: Dogs are wonderful",
+        answers: [
+            {
+                type: "Hate",
+                content: "False and I'm unhappy with my life"
+            },
+            {
+                type: "Middle",
+                content: "Dogs are fine, like my personality"
+            },
+            {
+                type: "Love",
+                content: "True, absolute wonders"
+            }
+        ]
+    },
+    {
+        question: "Dogs are kind, loyal and compassionate creatures. Are you?",
+        answers: [
+            {
+                type: "Middle",
+                content: "This is the strangest dog related quiz I've every taken."
+            },
+            {
+                type: "Love",
+                content: "I try to be every day"
+            },
+            {
+                type: "Hate",
+                content: "No I'm cruel, fickle and disgusting"
+            }
+        ]
+    },
+];
+
+export default quizQuestions;
